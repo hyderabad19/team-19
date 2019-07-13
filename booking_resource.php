5759a855739137e8fa93fbb7c171b0c69df57cfc
Lakshmi Vyshnavi <lakshmivyshnavi213@gmail.com>
2019-07-14 02:08:08 +0530

    booking slots with filters updated successfully

--- booking_resource.php
+++ booking_resource.php
@@ -1,65 +1,62 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "loop_19";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$cluster_id=$_SESSION('school_id');
-$cluster_id=$conn->query("select cluster_id from cluster where schoole_id="'.$school_id.'"");
-if ( isset( $_POST['apply_filters'] ) ) {
+session_start();
+$school_id=$_SESSION['school_id'];
+$con = mysqli_connect("localhost","root","","loop_19");
+	if(!$con)
+	{
+		echo "not connected";
+	}
+$cluster_id=mysqli_query($con,"select cluster_id from cluster where school_id=".$school_id.";");
+if(isset( $_POST['submit'])){
 
 // retrieve the form data by using the element's name attributes value as key
 
-
-$resource = $_REQUEST['resource'];
-
+$resource = $_REQUEST['resources'];
+echo "hii";
+echo $resource;
 	$date = $_REQUEST['date'];
 	if(is_null($date)){
 		$date=date("Ymd");
 	}
-
-$sql = "SELECT * FROM cluster innerjoin "'.$resource.'" on cluster.school_id== "'.$resource.'".school_id where date ="'.$date.'" and cluster_id="'.$cluster_id.'"";
-
-$result = $conn->query($sql);
+$sql = "SELECT * FROM cluster inner join ".$resource." on cluster.school_id=".$resource.".school_id where date ='$date' and cluster_id='$cluster_id';";
+$result = mysqli_query($con,$sql);
 echo "<table border='1'>";
-if ($result->num_rows > 0) {
+if (mysqli_num_rows($result)>0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    	if($row["capacity"]>0){
-    	<tr>
+    	if($row["capacity"] && $resource=$row['resource_name'])
+		{
+    	"<tr>";
     	echo "<td>".$row['school_id']."</td>";
 		echo "<td>".$row['school_name']."</td>";
-		echo "<td>".$row['strat_time']."</td>";
+		echo "<td>".$row['start_time']."</td>";
 		echo "<td>".$row['end_time']."</td>";
 		echo "<td>".$row['capacity']."</td>";
 		echo "<td>".$row['resource_name']."</td>";
 		echo "<td><a href='update.php?id=".$row['school_id']."&date=".$date."&start_time=".$row["start_time"]."&end_time=".$row["end_time"]."'>submit</a></td>";
-		</tr>    
+		"</tr>";    
     }}
 } else {
     echo "0 results";
 }
 }
 else{
-$resource = array("playground","library","activity_room","lab","equipment","teachers");
-$date=date("Ymd");
-for($i=0;$i<count($resource);i++){
+$resource = array("playground_db","library_db","activity_room_db","lab_db","equipment_db","part_time_teachers_db");
+$date=date("2019-07-13");
+for($i=0;$i<count($resource);$i++)
+{
+$sql="SELECT * FROM cluster inner join ".$resource[$i]." on cluster.school_id= ".$resource[$i].".school_id where date ='$date' and cluster_id=0;";
+$result = mysqli_query($con,$sql);
 
-$sql = "SELECT * FROM cluster innerjoin "$resource[i]" on cluster.school_id== "$resource[i]".school_id where date ="$date" and cluster_id="$cluster_id" ";
+echo "<table border='1'>";
 
-$result = $conn->query($sql);
-echo "<table border='1'>z";
-if ($result->num_rows > 0) {
+if (mysqli_num_rows($result)>0) 
+{
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    while($row = $result->fetch_assoc()) 
+	{
     	if($row["capacity"]>0){
-    	<tr>
+    	"<tr>";
         echo "<td>".$row['school_id']."</td>";
 		echo "<td>".$row['school_name']."</td>";
 		echo "<td>".$row['start_time']."</td>";
@@ -67,16 +64,13 @@ if ($result->num_rows > 0) {
 		echo "<td>".$row['capacity']."</td>";
 		echo "<td>".$row['resource_name']."</td>";
 		echo "<td><a href='update.php?id=".$row['school_id']."&date=".$date."&start_time=".$row["start_time"]."&end_time=".$row["end_time"]."'>submit</a></td>";
-		</tr>    
+		"</tr>";    
     }}
 } else {
     echo "0 results";
 }
 
 }
-
-
-$conn->close();
-
-
+}
+$con->close();
 ?>
