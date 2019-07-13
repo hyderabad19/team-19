c103a5a8cb6c7587590348ef244fd68bc56d10d5
saisagar1998 <saisagar071998@gmail.com>
2019-07-13 22:42:08 +0530

    scheduing personnel slots

--- displayslots.php
+++ displayslots.php
@@ -3,20 +3,6 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "loop_19";
-echo "<form action="/booking_resource.php" method='post'>
-  <select name="resources">
-    <option value="playground">playground</option>
-    <option value="library">library</option>
-    <option value="activity_room">activity_room</option>
-    <option value="lab">lab</option>
-    <option value="equipment">equipment</option>
-    <option value="part_time_teachers">part_time_teachers</option>
-  </select>
-  <br><br>
-  <input type="date">date</date>
-
-  <input type="submit">
-</form>"
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -24,9 +10,9 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $school_id=$_SESSION['school_id'];
-$cluster_id=$conn->query("select cluster_id from cluster where schoole_id="'.$school_id.'"");
+$cluster_id=$conn->query("select cluster_id from cluster where school_id="'.$school_id.'"");
 $resource = array("playground","library","activity_room","lab","equipment","teachers");
-$date=date("Ymd");
+$date=$_REQUEST['date'];
 for($i=0;$i<count($resource);i++){
 
 $sql = "SELECT * FROM cluster innerjoin "'.$resource[i].'" on cluster.school_id== "'.$resource[i].'".school_id where date ="'.$date.'" and cluster_id="'.$cluster_id.'"";
