7b69359e3ebd4f70e8976f705fcdab8c861cb51e
K-ADITYA <35301304+K-ADITYA@users.noreply.github.com>
2019-07-13 20:39:52 +0530

    completed

--- login.php
+++ login.php
@@ -5,22 +5,16 @@ error_reporting(0);
 	{
 		echo "not connected";
 	}
-	else
-	{
-	echo "connected..."."<br>";
-	}
 	$names=$_POST['user_id'];
 	$pass=$_POST['password'];
 	if(isset($_POST['submit']))
 	{
 		$result1=mysqli_query($con,"select * from login_schools where user_id='$names' and password='$pass';");
+		if($names=="admin" && $pass=="admin")
+			Header('Location:admin.php');	
 		if(mysqli_num_rows($result1)>0)
-		{
-			Header('Location:hello.php');
-		}
-			else
-			{
-				echo "Incorrect Credentials..Please re-enter the details";
-			}
+			Header('Location:school.php');
+		else
+			echo "Incorrect Credentials..Please re-enter the details";
 	}
 ?>
