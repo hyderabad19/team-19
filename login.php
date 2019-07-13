50b90f2948df471136d9aafba21bbbc4b171c987
Lakshmi Vyshnavi <lakshmivyshnavi213@gmail.com>
2019-07-14 01:15:22 +0530

    Further changes to be done

--- login.php
+++ login.php
@@ -1,23 +1,19 @@
 <?php
-error_reporting(0);
-session_start();
-
+	error_reporting(0);
+	session_start();
 	$con = mysqli_connect("localhost","root","","loop_19");
 	if(!$con)
 	{
 		echo "not connected";
 	}
-
 	$names=$_POST['user_id'];
 	$_SESSION['school_id']=$names;
 	$pass=$_POST['password'];
 	if(isset($_POST['submit']))
 	{
 		$result1=mysqli_query($con,"select * from login_schools where user_id='$names' and password='$pass';");
-		if($names=="admin" && $pass=="admin")
-			Header('Location:admin.php');	
-		if(mysqli_num_rows($result1)>0)
-			Header('Location:school.php');
+		if($result1)
+			Header('Location:onbookclick.php');
 		else
 			echo "Incorrect Credentials..Please re-enter the details";
 	}
--- onbookclick.php
+++ onbookclick.php
@@ -1,9 +1,8 @@
-<?
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "loop_19";
-echo "<form action="/booking_resource.php">
+<?php
+session_start();
+$school_id=$_SESSION['school_id'];
+?>
+<form action="booking_resource.php"method="POST">
   <select name="resources">
     <option value="playground">playground</option>
     <option value="library">library</option>
@@ -13,41 +12,47 @@ echo "<form action="/booking_resource.php">
     <option value="part_time_teachers">part_time_teachers</option>
   </select>
   <br><br>
-  <input type="date">date</date>
+  <input type="date">date
+  <input type="submit" name="submit">
+</form>
+<?php
 
-  <input type="submit">
-</form>"
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$school_id=$_SESSION['school_id'];
-$cluster_id=$conn->query("select cluster_id from cluster where schoole_id="'.$school_id.'"");
-$resource = array("playground","library","activity_room","lab","equipment","teachers");
-$date=date("Ymd");
-for($i=0;$i<count($resource);i++){
+$con = mysqli_connect("localhost","root","","loop_19");
+	if(!$con)
+	{
+		echo "not connected";
+	}
+$cluster_id=mysqli_query($con,"select cluster_id from cluster where school_id='school_id'");
+$resource = array("playground_db","library_db","activity_room_db","lab_db","equipment_db","part_time_teachers_db");
+$date=date("2019-07-13");
+echo $date;
+for($i=0;$i<count($resource);$i++)
+{
+$sql="SELECT * FROM cluster inner join ".$resource[$i]." on cluster.school_id= ".$resource[$i].".school_id where date ='$date' and cluster_id=0;";
+$result = mysqli_query($con,$sql);
 
-$sql = "SELECT * FROM cluster innerjoin "'.$resource[i].'" on cluster.school_id== "'.$resource[i].'".school_id where date ="'.$date.'" and cluster_id="'.$cluster_id.'"";
-
-$result = $conn->query($sql);
 echo "<table border='1'>";
-if ($result->num_rows > 0) {
+
+if (mysqli_num_rows($result)>0) 
+{
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-    	if($row["capacity"]>0){
-    	<tr>
-        echo "<td>".$row['school_id']."</td>";
-		echo "<td>".$row['school_name']."</td>";
-		echo "<td>".$row['start_time']."</td>";
-        echo "<td>".$row['end_time']."</td>";
-		echo "<td>".$row['capacity']."</td>";
-		echo "<td>".$row['resource_name']."</td>";
-		echo "<td><a href='update.php?id=".$row['school_id']."&date=".$date."&start_time=".$row["start_time"]."&end_time=".$row["end_time"]."'>submit</a></td>";
-		</tr>    
-    }}
-} else {
-    echo "0 results";
+    while($row = $result->fetch_assoc()) 
+	{
+    	if($row['capacity'])
+		{
+			"<tr>";
+			echo "<td>".$row['school_id']."</td>";
+			echo "<td>".$row['school_name']."</td>";
+			echo "<td>".$row['start_time']."</td>";
+			echo "<td>".$row['end_time']."</td>";
+			echo "<td>".$row['capacity']."</td>";
+			echo "<td>".$row['resource_name']."</td>";
+			echo "<td><a href='update.php?id=".$row['school_id']."&date=".$date."&start_time=".$row["start_time"]."&end_time=".$row["end_time"]."'>submit</a></td>";
+			"</tr>"; 
+		}
+	}
+} 
+
+
 }
 ?>
