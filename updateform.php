4a97b39c37b72f4b5bc6c348a7400b829d79c3df
saisagar1998 <saisagar071998@gmail.com>
2019-07-14 10:04:02 +0530

    changed data base name

--- updateform.php
+++ updateform.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "feedback";
+$dbname = "loop_19";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
